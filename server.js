d39b882df6915cef1d2a56656aa85a1f851a196a
Ruying <rychenf1@gmail.com>
2019-12-07 20:07:08 -0800

    list of users take turns

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ server.listen(port, () => {
 app.use(express.static(path.join(__dirname, "public")));
 
 var users = [];
+var canvas = [];
+let curTurnIdx = 0;
 
 io.on("connection", function (socket) {
     //io.emit('userlist', users);
@@ -21,14 +23,24 @@ io.on("connection", function (socket) {
         socket.userName = name;
 
         // user automatically joins a room under their own name
-        socket.join(name);
+        // socket.join(name);
         console.log(socket.userName + ' has joined. ID: ' + socket.id);
 
         // save the name of the user to an array called users
-        users.push(socket.userName);
+        users.push(socket.id);
+        console.log(users);
+        console.log(users.indexOf(socket.id));
 
+        if(users.indexOf(socket.id)==curTurnIdx){
+            // socket.join('drawer');
+            io.to(socket.id).emit('your turn');
+        }
+        else{
+            // socket.join('guesser');
+        }
+      
         // update all clients with the list of users
-		io.emit('userlist', users);
+		// io.emit('userlist', users);
 		
 
     })
@@ -39,6 +51,14 @@ io.on("connection", function (socket) {
 
     socket.on("correct answer", function (msg) {
         io.emit("correct answer", msg)
+    })
+
+    socket.on('next round',function(){
+        io.to(users[curTurnIdx]).emit('not your turn');
+        console.log('drawer becomes a guesser');
+        curTurnIdx = (curTurnIdx+1)%users.length;
+        io.to(users[curTurnIdx]).emit('your turn');
+        console.log('next guesser becomes the drawer');
     })
 
     socket.on('draw', function (line) {
